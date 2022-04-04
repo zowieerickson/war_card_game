f03eb2ec5fcbe66a0cb969f8d1bf61fef2072ff4
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-04 16:59:11 -0400

    fetching deckofcards api, holding deck_id globally

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('sup')
+let deckId;
+
+function handleClick() {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+            console.log(deckId)
+        })
+}
+setTimeout(() => console.log(deckId), 5000)
+document.querySelector("#new-deck").addEventListener("click", handleClick)
