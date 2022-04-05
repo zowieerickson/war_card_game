b08c48e35a1ca3a9294f2357be02db623246ed5e
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-04 22:51:33 -0400

    adding basic styles

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function handleClick() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
-            console.log(deckId)
+            document.querySelector("#draw-cards").disabled = false;
         })
 }
 
