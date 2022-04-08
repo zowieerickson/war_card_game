16353b1030393472b1e10fa1311c3d6c08f09bb1
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-08 09:13:00 -0400

    Feature: reset game when clicking new deck button

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let deckId;
 const newDeckBtn = document.querySelector("#new-deck");
 const drawCardsBtn = document.querySelector("#draw-cards");
 const remainingCards = document.querySelector("#remaining-cards");
+const winnerEle = document.querySelector("#winner-msg");
+const cardsContainer = document.querySelector("#container-cards");
+
 let userScore = 0;
 let computerScore = 0;
 
@@ -12,8 +15,25 @@ function handleClick() {
             deckId = data.deck_id;
             drawCardsBtn.disabled = false;
             drawCardsBtn.classList.remove('disabled');
-            remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`
+            remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`;
+
+            resetGame();
         })
+}
+
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+
+
+    winnerEle.textContent = "Game of War"
+    document.querySelector("#computer-score").textContent = computerScore;
+    document.querySelector("#user-score").textContent = userScore;
+
+    for (let i = 0; i < cardsContainer.children.length; i ++) {
+        cardsContainer.children[i].innerHTML = ``
+    }
+
 }
 
 
@@ -22,19 +42,27 @@ function drawCards() {
         .then(response => response.json())
         .then(data => {
             remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`
-            if (data.remaining === 0) {
-                drawCardsBtn.disabled = true;
-                drawCardsBtn.classList.add('disabled');
-            }
 
-            const cardsContainer = document.querySelector("#container-cards");
+
             for (let i = 0; i < cardsContainer.children.length; i ++) {
                 cardsContainer.children[i].innerHTML = `
                     <img src=${data.cards[i].image} />
                 `
             }
             const winnerText = handleCards(data.cards[0], data.cards[1])
-            document.querySelector("#winner-msg").textContent = winnerText
+            winnerEle.textContent = winnerText
+
+            if (data.remaining === 0) {
+                if (userScore > computerScore) {
+                    winnerEle.textContent = 'You won the game of war!'
+                } else if (userScore < computerScore) {
+                    winnerEle.textContent = "The computer has won the game of war!"
+                } else {
+                    winnerEle.textContent = 'The game ends in a tie!'
+                }
+                drawCardsBtn.disabled = true;
+                drawCardsBtn.classList.add('disabled');
+            }
         })
 }
 
