12dea3b4d79651b39c3578f47975845d59076759
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-08 10:23:55 -0400

    refactored api requests to use async/await

--- index.js
+++ index.js
@@ -8,62 +8,40 @@ const cardsContainer = document.querySelector("#container-cards");
 let userScore = 0;
 let computerScore = 0;
 
-function handleClick() {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-        .then(response => response.json())
-        .then(data => {
-            deckId = data.deck_id;
-            drawCardsBtn.disabled = false;
-            drawCardsBtn.classList.remove('disabled');
-            remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`;
-
-            resetGame();
-        })
-}
-
-function resetGame() {
-    userScore = 0;
-    computerScore = 0;
-
-
-    winnerEle.textContent = "Game of War"
-    document.querySelector("#computer-score").textContent = computerScore;
-    document.querySelector("#user-score").textContent = userScore;
-
-    for (let i = 0; i < cardsContainer.children.length; i ++) {
-        cardsContainer.children[i].innerHTML = ``
+async function handleClick() {
+   const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+   const data = await response.json();
+        deckId = data.deck_id;
+        drawCardsBtn.disabled = false;
+        drawCardsBtn.classList.remove('disabled');
+        remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`;
+        resetGame();
     }
 
-}
+async function drawCards() {
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
+    const data = await response.json();
+        remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`
 
+        for (let i = 0; i < cardsContainer.children.length; i ++) {
+            cardsContainer.children[i].innerHTML = `
+                <img src=${data.cards[i].image} />
+            `
+        }
+        const winnerText = handleCards(data.cards[0], data.cards[1])
+        winnerEle.textContent = winnerText
 
-function drawCards() {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-        .then(response => response.json())
-        .then(data => {
-            remainingCards.innerHTML = `Remaining cards: <span>${data.remaining}</span>`
-
-
-            for (let i = 0; i < cardsContainer.children.length; i ++) {
-                cardsContainer.children[i].innerHTML = `
-                    <img src=${data.cards[i].image} />
-                `
+        if (data.remaining === 0) {
+            if (userScore > computerScore) {
+                winnerEle.textContent = 'You won the game of war!'
+            } else if (userScore < computerScore) {
+                winnerEle.textContent = "The computer has won the game of war!"
+            } else {
+                winnerEle.textContent = 'The game ends in a tie!'
             }
-            const winnerText = handleCards(data.cards[0], data.cards[1])
-            winnerEle.textContent = winnerText
-
-            if (data.remaining === 0) {
-                if (userScore > computerScore) {
-                    winnerEle.textContent = 'You won the game of war!'
-                } else if (userScore < computerScore) {
-                    winnerEle.textContent = "The computer has won the game of war!"
-                } else {
-                    winnerEle.textContent = 'The game ends in a tie!'
-                }
-                drawCardsBtn.disabled = true;
-                drawCardsBtn.classList.add('disabled');
-            }
-        })
+            drawCardsBtn.disabled = true;
+            drawCardsBtn.classList.add('disabled');
+        }
 }
 
 newDeckBtn.addEventListener("click", handleClick);
@@ -77,7 +55,6 @@ function handleCards(card1, card2) {
  
     if (card1Value > card2Value) {
         computerScore++
-        console.log(computerScore)
         document.querySelector("#computer-score").textContent = computerScore;
         return 'Computer wins!';
     } else if (card1Value < card2Value) {
@@ -86,5 +63,18 @@ function handleCards(card1, card2) {
         return 'You win!';
     } else {
         return 'War!';
+    }
+}
+
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+
+    winnerEle.textContent = "Game of War"
+    document.querySelector("#computer-score").textContent = computerScore;
+    document.querySelector("#user-score").textContent = userScore;
+
+    for (let i = 0; i < cardsContainer.children.length; i ++) {
+        cardsContainer.children[i].innerHTML = ``
     }
 }
